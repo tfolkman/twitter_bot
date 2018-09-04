24356acbe8ecf170161e870c1f3752bf8430022e
Tyler Folkman <tylerfolkman@gmail.com>
2018-09-03 22:00:08 -0600

    more utils

--- bot/common/util.py
+++ bot/common/util.py
@@ -3,9 +3,10 @@ util.py
 ====================================
 Contains common utils used across the code
 """
+from typing import List, Tuple
+import re
 
-
-def read_lines(filename: str) -> object:
+def read_lines(filename: str) -> List[str]:
     """
 
     Parameters
@@ -18,3 +19,51 @@ def read_lines(filename: str) -> object:
 
     """
     return open(filename).read().split('\n')[:-1]
+
+
+def filter_line(line: str) -> str:
+    """
+    Removes any non-alphanumeric characters from a string
+    Parameters
+    ----------
+    line : str
+        The string you want filtered
+
+    Returns
+    -------
+    str
+        The line with non-alphanumerics filtered out
+
+    """
+    return re.sub(r'[^a-zA-Z0-9_\s]+', "", line)
+
+
+def filter_data(sequences: List[str], min_length: int = 3, max_length: int = 20) -> Tuple[List[str], List[str]]:
+    """
+
+    Parameters
+    ----------
+    sequences : List[str]
+        List of sting sequences where evens are queries and odds answers
+    min_length: int
+        Min length of a query or answer
+    max_length: int
+        Max length of a query or answer
+
+    Returns
+    -------
+    Tuple[List[str], List[str]]
+        The filtered queries, the filtered answers
+
+    """
+    filtered_query, filtered_answer = [], []
+    # even rows are queries and odd are answers
+    for i in range(0, len(sequences), 2):
+        query = sequences[i]
+        answer = sequences[i+1]
+        query_length = len(query.split())
+        answer_length = len(answer.split())
+        if min_length <= query_length <= max_length and min_length <= answer_length <= max_length:
+            filtered_query.append(query)
+            filtered_answer.append(answer)
+    return filtered_query, filtered_answer
--- bot/tests/common/test_util.py
+++ bot/tests/common/test_util.py
@@ -13,3 +13,20 @@ def test_read_lines():
     assert lines == ["yeah i'm preparing myself to drop a lot on this man, but definitely need something reliable",
                     "yeah dude i would definitely consider a daniel defence super reliable and they are just bad ass",
                      "i'm about to meet my mans ex friend with benefit, tune in next week to see if i have to put hands on"]
+
+
+def test_filter_line():
+    filtered = util.filter_line("^&twitter is %awesome!!")
+    assert filtered == "twitter is awesome"
+
+
+def test_filter_data():
+    rows = ["yeah i'm preparing myself to drop a lot on this man, but definitely need something reliable",
+                     "yeah dude i would definitely consider a daniel defence super reliable and they are just bad ass",
+                     "this is a good questions",
+                     "short answer",
+                     "l l l l l l l l l l l l l l l l l l l l l",
+                     "s s s s"]
+    filtered_queries, filtered_answers = util.filter_data(rows)
+    assert filtered_queries == ["yeah i'm preparing myself to drop a lot on this man, but definitely need something reliable"]
+    assert filtered_answers == ["yeah dude i would definitely consider a daniel defence super reliable and they are just bad ass"]
